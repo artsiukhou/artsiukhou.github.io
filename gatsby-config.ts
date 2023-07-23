b03754c5a56652f331afde1d52fa15b4520e6a4a
Vova Artsiukhou <>
2023-07-23 20:12:56 +0100

    Tuned gatsby-config.js, rewrote welcome msg, removed projects

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -8,14 +8,14 @@ const config: GatsbyConfig = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-minimal-blog/gatsby-config.mjs
-    siteTitle: `Minimal Blog`,
-    siteTitleAlt: `Minimal Blog - Gatsby Theme`,
-    siteHeadline: `Minimal Blog - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://minimal-blog.lekoarts.de`,
-    siteDescription: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and line highlighting.`,
-    siteImage: `/banner.jpg`,
+    siteTitle: `Vova's Blog`,
+    siteTitleAlt: `Vova's Blog`,
+    siteHeadline: `Vova's Blog -- thoughts about software engineering`,
+    siteUrl: `https://artsiukhou.github.io/`,
+    siteDescription: `Vova's Blog -- thoughts about software engineering`,
+    // siteImage: `/banner.jpg`,
     siteLanguage: `en`,
-    author: `@lekoarts_de`,
+    author: `Vova`,
   },
   trailingSlash: `never`,
   plugins: [
@@ -24,6 +24,10 @@ const config: GatsbyConfig = {
       // See the theme's README for all available options
       options: {
         navigation: [
+          {
+            title: `Home`,
+            slug: `/`,
+          },
           {
             title: `Blog`,
             slug: `/blog`,
@@ -35,12 +39,12 @@ const config: GatsbyConfig = {
         ],
         externalLinks: [
           {
-            name: `Twitter`,
-            url: `https://twitter.com/lekoarts_de`,
+            name: `GitHub`,
+            url: `http://github.com/artsiukhou`,
           },
           {
-            name: `Homepage`,
-            url: `https://www.lekoarts.de?utm_source=minimal-blog&utm_medium=Starter`,
+            name: `LinkedIn`,
+            url: `https://www.linkedin.com/in/vladimir-artyukhov/`,
           },
         ],
       },
@@ -54,9 +58,9 @@ const config: GatsbyConfig = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
-        short_name: `minimal-blog`,
-        description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
+        name: `Vova's Blog`,
+        short_name: `Vova's Blog`,
+        description: `Vova's Blog -- thoughts about software engineering`,
         start_url: `/`,
         background_color: `#fff`,
         // This will impact how browsers show your PWA/website
@@ -64,16 +68,16 @@ const config: GatsbyConfig = {
         // theme_color: `#6B46C1`,
         display: `standalone`,
         icons: [
-          {
-            src: `/android-chrome-192x192.png`,
-            sizes: `192x192`,
-            type: `image/png`,
-          },
-          {
-            src: `/android-chrome-512x512.png`,
-            sizes: `512x512`,
-            type: `image/png`,
-          },
+          // {
+          //   src: `/android-chrome-192x192.png`,
+          //   sizes: `192x192`,
+          //   type: `image/png`,
+          // },
+          // {
+          //   src: `/android-chrome-512x512.png`,
+          //   sizes: `512x512`,
+          //   type: `image/png`,
+          // },
         ],
       },
     },
